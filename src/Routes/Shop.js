e00bbf8a9827c670b376ae3f737b589d5f5859a3
Sophia Amin <samin@shipt.com>
2019-11-30 23:29:57 -0800

    everything needed is in the console. cant render

--- src/Routes/Shop.js
+++ src/Routes/Shop.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import '../App.css';
 import {Link} from 'react-router-dom';
 //  use state: hold the info from api 
@@ -6,47 +6,85 @@ import {Link} from 'react-router-dom';
 
 function Shop() {
 
-  const eachItem = [];
+  let eachItem = [];
 
-  function itemList(products){
-    let itemsArray=products
-    console.log("itemArray", itemsArray);
+  const itemName = function listItem(products){
+    var name = products.map(function(prod){
+      console.log("itemer", prod.name);
+      return prod.name;
+    });
+    console.log("did this work", name);
     return(
-      <h1>Hi</h1>
-    )
+      name
+    );
    }
 
-   function fetchItems(){
-      fetch('https://api.bestbuy.com/v1/products(search=toaster&search=stainless&search=steel)?format=json&show=sku,name,image,salePrice&apiKey=mznufc3sxam3w2r7pkdbmnu2')
+   function RenderName(nname){
+    return(
+      <div>
+        <h1>{nname[0]}</h1>
+      </div>
+    );
+   }
+
+  function ItemList(products){
+    let itemsArray=products
+    console.log("itemArray2", itemsArray);
+    eachItem = itemsArray;
+    return (
+      <h1>Hola</h1>
+    );
+   }
+
+  // function Card(props){
+  //   return ( 
+  //     <div>
+  //     <h1>Hola</h1>
+  //   <h1>{props.name}</h1>
+  //   </div>
+  //   );
+  // }
+
+  //  function itemList(product) {
+  //   return (
+  //     <Card
+  //       key={product.sku}
+  //       name={product.name}
+  //     />
+  //   );
+  // }
+
+   function getItems() { fetch('https://api.bestbuy.com/v1/products(search=toaster&search=stainless&search=steel)?format=json&show=sku,name,image,salePrice&apiKey=mznufc3sxam3w2r7pkdbmnu2')
     .then(response => {
       return response.json()
     })
     .then(data => {
-      eachItem.allProducts = data.products
-      itemList(eachItem.allProducts);
-      // ItemList(eachItem);
-      console.log("allprods", eachItem.allProducts)
-      return eachItem.allProducts;
+     let prod = data.products
+      // itemList(eachItem.allProducts);
+      console.log("allprods", prod)
+      // itemList(prod[0]);
+      itemName(prod);
+      ItemList(prod);
+      RenderName(prod);
+      return prod;
     })
     .catch(err => {
       // Do something for an error here
-    })
-   }
-   
-  
-  let itemsArray = fetchItems();
-  console.log("itemsArray OUTSIDE", itemsArray);
-  
-
+    });
+  }
+  getItems();
   return (
     <>
     <h1>Shop Page</h1>
     <div>
       <h1>Hi</h1>
-      <h1>{itemsArray.map(itemList)}</h1>
+      <RenderName />
+      <ItemList />
+      <itemName />
     </div>
     </>
   );
 }
+
 
 export default Shop;
